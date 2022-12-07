04eeb03b8b7d4abeb0aa1b1b2be6f86e44286fca
HiteshP21 <pant.hit@northeastern.edu>
2022-12-06 19:05:04 -0500

    Database-DB4O support added

---
+++ Agriculture_Mgmt_system/src/Model/DB4OUtil/DB4OUtil.java
@@ -0,0 +1,50 @@
+package Model.DB4OUtil;
+import com.db4o.Db4oEmbedded;
+import com.db4o.ObjectContainer;
+import com.db4o.ObjectSet;
+import com.db4o.config.EmbeddedConfiguration;
+import com.db4o.ta.TransparentPersistenceSupport;
+
+import java.nio.file.Paths;
+
+
+
+
+
+public class DB4OUtil {
+    
+    
+    private static final String FILENAME = Paths.get("Databank.db4o").toAbsolutePath().toString();// path to the data store
+    private static DB4OUtil dB4OUtil;
+    
+    public synchronized static DB4OUtil getInstance(){
+        if (dB4OUtil == null){
+            dB4OUtil = new DB4OUtil();
+        }
+        return dB4OUtil;
+    }
+
+    protected synchronized static void shutdown(ObjectContainer conn) {
+        if (conn != null) {
+            conn.close();
+        }
+    }
+
+    private ObjectContainer createConnection() {
+        try {
+
+            EmbeddedConfiguration config = Db4oEmbedded.newConfiguration();
+            config.common().add(new TransparentPersistenceSupport());
+            //Controls the number of objects in memory
+            config.common().activationDepth(Integer.MAX_VALUE);
+            //Controls the depth/level of updation of Object
+            config.common().updateDepth(Integer.MAX_VALUE);
+
+            ObjectContainer db = Db4oEmbedded.openFile(config, FILENAME);
+            return db;
+        } catch (Exception ex) {
+            System.out.print(ex.getMessage());
+        }
+        return null;
+    }
+}
