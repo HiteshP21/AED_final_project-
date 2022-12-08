3527ade1157a9d19d7465da669f90288d6b6f0f2
HiteshP21 <pant.hit@northeastern.edu>
2022-12-07 21:53:37 -0500

    Emp updated

---
+++ Agriculture_Mgmt_system/src/Model/Emp/Emp.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model.Emp;
+
+/**
+ *
+ * @author Hitesh
+ */
+public class Emp {
+        private String name;
+    private int id;
+    private static int count = 1;
+
+    public Emp() {
+        id = count;
+        count++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+    public String getName() {
+        return name;
+    }
+    
+
+    //Override method to get string of an object
+    @Override
+    public String toString() {
+        return name;
+    }
+    
+}
---
+++ Agriculture_Mgmt_system/src/Model/WorkQueue/YieldRequest.java
@@ -0,0 +1,20 @@
+
+package Model.WorkQueue;
+
+/**
+ *
+ * @author Hitesh
+ */
+public class YieldRequest extends WorkRequest{
+    
+    private String testingOutput;
+
+    public String getTestingOutput() {
+        return testingOutput;
+    }
+
+    public void setTestingOutput(String testingOutput) {
+        this.testingOutput = testingOutput;
+    }
+    
+}
